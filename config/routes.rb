a3de91c09e35b0e87bd3e74c78fd3221a1466a20
Martin Fencl <martin.fencl@gmail.com>
2016-10-30 20:04:49 -0500

    wire up and named the index and show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'students/index'
+  # routes
+  # HTTP VERBS: get, post, put, patch, delete
 
-  get 'students/show'
+  # verb 'url/path' => 'controller#action'
+
+  get 'students'      => 'students#index'
+  get 'students/:id'  => 'students#show', as: 'student', id: /\d+/
+  # same as:
+  # resources 'students', only: [:index, :show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
