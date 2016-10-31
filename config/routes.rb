30cd1be865c6ed4ed23e8b1ba9df9f0f78fe6b2b
Martin Fencl <martin.fencl@gmail.com>
2016-10-31 13:24:16 -0500

    Create root path to students#index

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   # same as:
   # resources 'students', only: [:index, :show]
 
+  root 'students#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
