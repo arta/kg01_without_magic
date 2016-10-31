43776768ffdd578fe92804d81918234bb1c8633e
Martin Fencl <martin.fencl@gmail.com>
2016-10-31 17:25:09 -0500

    Implement and route student#new and student#create actions

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,7 +6,30 @@ class StudentsController < ApplicationController
   # Automatically load the app/views/students/index.html.erb
 
   def show
-    @student = Student.find( params[:id] ) # Load the student mentioned by id in the route 
+    @student = Student.find( params[:id] ) # Load the student mentioned by id in the route
   end
   # Automatically load the app/views/students/show.html.erb
+
+  def new
+    @student = Student.new
+  end
+  # Automatically load the app/views/students/new.html.erb
+
+  def create
+    @student = Student.new( student_params )
+
+    if @student.save # If the validations pass
+      redirect_to @student # Redirect to show action for this student
+    else # If the validations fail
+      render :new # Render the view associated with the new action.
+    end
+  end
+  # This action does not have an associated view!
+  # Automatically load the app/views/students/:id ?
+
+  private
+  def student_params
+    params.require( :student ).permit( :name )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
   # routes
   # HTTP VERBS: get, post, put, patch, delete
   # verb 'url/path' => 'controller#action'
+  # SHOW Routes:
   get 'students'      => 'students#index'
   get 'students/:id'  => 'students#show', as: 'student', id: /\d+/ #<= constraints: http://guides.rubyonrails.org/routing.html#segment-constraints
   # same as:
   # resources 'students', only: [:index, :show]
 
+  # CREATE Routes:
+  get 'students/new'  => 'students#new', as: 'new_student'
+  post 'students'     => 'students#create'
   root 'students#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
